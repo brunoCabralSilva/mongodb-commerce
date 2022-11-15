c8b12815947193d361da664a7809bd2c1452df00
Bruno Cabral <bruno.cabral.silva2018@gmail.com>
2022-11-15 19:46:48 -0300

    requisito 16

---
+++ challenges/desafio16.js
@@ -0,0 +1,16 @@
+db.produtos.updateMany({
+  nome: "Big Mac",
+},
+  {
+    $currentDate: {
+      ultimaModificacao: { $type: "timestamp" },
+    },
+  });
+
+db.produtos.find({
+  ultimaModificacao: { $exists: true },
+},
+{
+  nome: 1,
+  _id: 0,
+});
