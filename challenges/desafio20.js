de6b11d5fe86474b8b914a1c4c45504104e8159b
Bruno Cabral <bruno.cabral.silva2018@gmail.com>
2022-11-16 15:29:37 -0300

    Correção do Requisito 20

--- challenges/desafio20.js
+++ challenges/desafio20.js
@@ -1,6 +1,5 @@
-db.produtos.updateMany({
-  nome: "Quarteirão com Queijo",
-},
+db.produtos.updateOne(
+{ nome: { $eq: "Quarteirão com Queijo" } },
 { $pop: { ingredientes: -1 } });
 
 db.produtos.find({}, 
